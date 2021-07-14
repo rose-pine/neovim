4bd3cf12faa6635feae42d1c1d6c680a97c1eac5
mvllow <mvllow@icloud.com>
2021-07-13 20:07:01 -0500

    tempfix: galaxyline breaks in LunarVim

--- lua/rose-pine/palette.lua
+++ lua/rose-pine/palette.lua
@@ -17,6 +17,37 @@ local palette = {
 	none = 'NONE',
 }
 
+local colors = {
+	base = '#191724',
+	surface = '#1f1d2e',
+	overlay = '#26233a',
+	inactive = '#555169',
+	subtle = '#6e6a86',
+	text = '#e0def4',
+	love = '#eb6f92',
+	gold = '#f6c177',
+	rose = '#ebbcba',
+	pine = '#31748f',
+	foam = '#9ccfd8',
+	iris = '#c4a7e7',
+	highlight = '#2a2837',
+	highlight_inactive = '#211f2d',
+	highlight_overlay = '#3a384a',
+	none = 'NONE',
+
+	alt_bg = palette.surface,
+	black = palette.overlay,
+	red = palette.love,
+	green = palette.pine,
+	orange = palette.gold,
+	yellow = palette.gold,
+	blue = palette.foam,
+	purple = palette.iris,
+	magenta = palette.iris,
+	cyan = palette.rose,
+	white = palette.text,
+}
+
 if vim.g.rose_pine_variant == 'dawn' then
 	palette = {
 		base = '#faf4ed',
@@ -34,6 +65,36 @@ if vim.g.rose_pine_variant == 'dawn' then
 		highlight = '#eee9e6',
 		highlight_inactive = '#f2ede9',
 		highlight_overlay = '#e4dfde',
+	}
+
+	colors = {
+		base = '#faf4ed',
+		surface = '#fffaf3',
+		overlay = '#f2e9de',
+		inactive = '#9893a5',
+		subtle = '#6e6a86',
+		text = '#575279',
+		love = '#b4637a',
+		gold = '#ea9d34',
+		rose = '#d7827e',
+		pine = '#286983',
+		foam = '#56949f',
+		iris = '#907aa9',
+		highlight = '#eee9e6',
+		highlight_inactive = '#f2ede9',
+		highlight_overlay = '#e4dfde',
+
+		alt_bg = palette.surface,
+		black = palette.overlay,
+		red = palette.love,
+		green = palette.pine,
+		orange = palette.gold,
+		yellow = palette.gold,
+		blue = palette.foam,
+		purple = palette.iris,
+		magenta = palette.iris,
+		cyan = palette.rose,
+		white = palette.text,
 	}
 elseif vim.g.rose_pine_variant == 'moon' then
 	palette = {
@@ -53,6 +114,36 @@ elseif vim.g.rose_pine_variant == 'moon' then
 		highlight_inactive = '#2a283d',
 		highlight_overlay = '#3f3c53',
 	}
+
+	colors = {
+		base = '#232136',
+		surface = '#2a273f',
+		overlay = '#393552',
+		inactive = '#59546d',
+		subtle = '#817c9c',
+		text = '#e0def4',
+		love = '#eb6f92',
+		gold = '#f6c177',
+		rose = '#ea9a97',
+		pine = '#3e8fb0',
+		foam = '#9ccfd8',
+		iris = '#c4a7e7',
+		highlight = '#312f44',
+		highlight_inactive = '#2a283d',
+		highlight_overlay = '#3f3c53',
+
+		alt_bg = palette.surface,
+		black = palette.overlay,
+		red = palette.love,
+		green = palette.pine,
+		orange = palette.gold,
+		yellow = palette.gold,
+		blue = palette.foam,
+		purple = palette.iris,
+		magenta = palette.iris,
+		cyan = palette.rose,
+		white = palette.text,
+	}
 end
 
-return palette
+return colors
