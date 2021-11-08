7f2bcbb369c1dad5a2627e6bcf0d351a47288835
mvllow <mvllow@icloud.com>
2021-11-08 16:23:45 -0600

    chore: update bufferline style

--- lua/rose-pine/theme.lua
+++ lua/rose-pine/theme.lua
@@ -294,7 +294,7 @@ theme.treesitter = {
 theme.plugins = {
 	-- barbar.nvim
 	-- https://github.com/romgrk/barbar.nvim
-	BufferTabpageFill = { bg = p.none },
+	BufferTabpageFill = { fg = p.base, bg = p.base },
 	BufferCurrent = { fg = p.text, bg = p.overlay },
 	BufferCurrentIndex = { fg = p.text, bg = p.overlay },
 	BufferCurrentMod = { fg = p.foam, bg = p.overlay },
@@ -302,11 +302,11 @@ theme.plugins = {
 	BufferInactive = { fg = p.subtle },
 	BufferInactiveIndex = { fg = p.subtle },
 	BufferInactiveMod = { fg = p.foam },
-	BufferInactiveSign = { fg = p.subtle },
+	BufferInactiveSign = { fg = p.inactive },
 	BufferVisible = { fg = p.subtle },
 	BufferVisibleIndex = { fg = p.subtle },
 	BufferVisibleMod = { fg = p.foam },
-	BufferVisibleSign = { fg = p.subtle },
+	BufferVisibleSign = { fg = p.inactive },
 
 	-- gitsigns.nvim
 	-- https://github.com/lewis6991/gitsigns.nvim
