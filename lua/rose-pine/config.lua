62567755fd9211c3e5da19e9a045f5a0c6c4ea01
not <mvllow@icloud.com>
2021-12-03 09:38:34 -0600

    Refactor structure (#43)
    * refactor: palette
    
    * refactor: simplify init
    
    * chore!: rename functions
    
    - `rose-pine.functions.select_variant` -> `rose-pine.set`
    - `rose-pine.functions.toggle_variant` -> `rose-pine.toggle`
    
    * docs: update options
    
    * chore: update config with shared headings

---
+++ lua/rose-pine/config.lua
@@ -0,0 +1,49 @@
+local palette = require('rose-pine.palette')
+
+local config = {
+	bold_vert_split = vim.g.rose_pine_bold_verical_split_line or false,
+	no_background = vim.g.rose_pine_disable_background or false,
+	no_italics = vim.g.rose_pine_disable_italics or false,
+	colors = {
+		punctuation = palette.subtle,
+		comment = palette.subtle,
+		hint = palette.iris,
+		info = palette.foam,
+		warn = palette.gold,
+		error = palette.love,
+
+		-- TODO: Expose these once matched with syntax and cmp kind
+		-- variable = '',
+		-- class = '',
+		-- interface = '',
+		-- ['function'] = '',
+		-- method = '',
+
+		---@type string|table<string, string>
+		headings = {
+			h1 = palette.foam,
+			h2 = palette.foam,
+			h3 = palette.foam,
+			h4 = palette.foam,
+			h5 = palette.foam,
+			h6 = palette.foam,
+		},
+	},
+}
+
+local colors = vim.g.rose_pine_colors or {}
+
+if type(colors.headings) == 'string' then
+	colors.headings = {
+		h1 = colors.headings,
+		h2 = colors.headings,
+		h3 = colors.headings,
+		h4 = colors.headings,
+		h5 = colors.headings,
+		h6 = colors.headings,
+	}
+end
+
+config.colors = vim.tbl_deep_extend('force', config.colors, colors)
+
+return config
--- lua/rose-pine/init.lua
+++ lua/rose-pine/init.lua
@@ -1,9 +1,74 @@
-local util = require('rose-pine.util')
-
 local M = {}
 
-function M.set()
-	util.load()
+function M.colorscheme()
+	if vim.g.colors_name then
+		vim.cmd('hi clear')
+	end
+
+	vim.opt.termguicolors = true
+	vim.g.colors_name = 'rose-pine'
+
+	-- Match terminal theme if no variant is set
+	if vim.g.rose_pine_variant == nil and vim.o.background == 'light' then
+		vim.g.rose_pine_variant = 'dawn'
+	elseif vim.g.rose_pine_variant == 'dawn' then
+		vim.o.background = 'light'
+	end
+
+	---@param group string
+	---@param color table<string, string>
+	local function highlight(group, color)
+		local style = color.style and 'gui=' .. color.style or 'gui=NONE'
+		local fg = color.fg and 'guifg=' .. color.fg or 'guifg=NONE'
+		local bg = color.bg and 'guibg=' .. color.bg or 'guibg=NONE'
+		local sp = color.sp and 'guisp=' .. color.sp or ''
+
+		local hl = 'highlight '
+			.. group
+			.. ' '
+			.. style
+			.. ' '
+			.. fg
+			.. ' '
+			.. bg
+			.. ' '
+			.. sp
+
+		vim.cmd(hl)
+		if color.link then
+			vim.cmd('highlight! link ' .. group .. ' ' .. color.link)
+		end
+	end
+
+	for group, colors in pairs(require('rose-pine.theme')) do
+		highlight(group, colors)
+	end
+
+	require('rose-pine.galaxyline.theme')
+end
+
+function M.set(variant)
+	vim.g.rose_pine_variant = variant
+	vim.cmd([[colorscheme rose-pine]])
+end
+
+function M.toggle(variants)
+	variants = variants or { 'main', 'moon', 'dawn' }
+
+	local index = {}
+	for k, v in pairs(variants) do
+		index[v] = k
+	end
+
+	if vim.g.rose_pine_current_variant == nil then
+		vim.g.rose_pine_current_variant = index[vim.g.rose_pine_variant] or 0
+	end
+
+	vim.g.rose_pine_current_variant = (
+			vim.g.rose_pine_current_variant % #variants
+		) + 1
+
+	M.set(variants[vim.g.rose_pine_current_variant])
 end
 
 return M
