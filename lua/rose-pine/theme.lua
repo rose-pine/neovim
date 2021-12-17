fbc31eb998a01dac2ff34b4bd36e7438fc7337b7
Samuel K <mrsosaki@posteo.net>
2021-12-17 10:00:59 +0100

    style(lspsaga): rearrange diagnostic highlights
    Rearrange `DiagnosticInformation` and `DiagnosticWarning` to their respective spots

--- lua/rose-pine/theme.lua
+++ lua/rose-pine/theme.lua
@@ -176,7 +176,9 @@ local theme = {
 
 	DiagnosticHint = { fg = config.colors.hint },
 	DiagnosticInfo = { fg = config.colors.info },
+	DiagnosticInformation = { link = 'DiagnosticInfo' },
 	DiagnosticWarn = { fg = config.colors.warn },
+	DiagnosticWarning = { link = 'DiagnosticWarn' },
 	DiagnosticError = { fg = config.colors.error },
 	DiagnosticDefaultHint = { fg = config.colors.hint },
 	DiagnosticDefaultInfo = { fg = config.colors.info },
@@ -413,9 +415,6 @@ local theme = {
 
 	-- LspSaga
 	-- https://github.com/tami5/lspsaga.nvim (fork of https://github.com/glepnir/lspsaga.nvim)
-	DiagnosticInformation = { link = 'DiagnosticInfo' },
-	DiagnosticWarning = { link = 'DiagnosticWarn' },
-
 	LspSagaCodeActionTitle = {
 		fg = p.rose,
 		style = 'bold',
