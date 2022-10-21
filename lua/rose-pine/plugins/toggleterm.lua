1b8b98e19413b5088ca0c72d1884e63cb8fac0a3
Eric Xiao <ericxiao0000@gmail.com>
2022-10-21 10:44:47 -0700

    format

--- lua/rose-pine/plugins/toggleterm.lua
+++ lua/rose-pine/plugins/toggleterm.lua
@@ -5,10 +5,10 @@
 --- local highlights = require('rose-pine.plugins.toggleterm')
 --- require('toggleterm').setup({ highlights = highlights })
 return {
-    Normal = {link = 'Normal'},
-    NormalFloat = {link = 'Normal'},
-    FloatBorder = {link = 'FloatBorder'},
-    SignColumn = {link = 'SignColumn'},
-    StatusLine = {link = 'StatusLine'},
-    StatusLineNC = {link = 'StatusLineNC'}
+  Normal = {link = 'Normal'},
+  NormalFloat = {link = 'Normal'},
+  FloatBorder = {link = 'FloatBorder'},
+  SignColumn = {link = 'SignColumn'},
+  StatusLine = {link = 'StatusLine'},
+  StatusLineNC = {link = 'StatusLineNC'}
 }
