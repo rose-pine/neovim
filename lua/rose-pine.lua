e43acd2e39a6ec68bb3cfae03840c5be18ee2fd5
mvllow <mvllow@icloud.com>
2024-01-09 15:17:04 -0600

    feat: improve neogit support

--- lua/rose-pine.lua
+++ lua/rose-pine.lua
@@ -387,10 +387,20 @@ local function set_highlights()
 		CmpItemKindSnippet = { link = "String" },
 		CmpItemKindVariable = { link = "Identifier" },
 
-		-- TimUntersberger/neogit
+		-- NeogitOrg/neogit
+		-- https://github.com/NeogitOrg/neogit/blob/master/lua/neogit/lib/hl.lua#L109-L198
+		NeogitChangeAdded = { fg = groups.git_add, bold = styles.bold, italic = styles.italic },
+		NeogitChangeBothModified = { fg = groups.git_change, bold = styles.bold, italic = styles.italic },
+		NeogitChangeCopied = { fg = palette.bg_cyan, bold = styles.bold, italic = styles.italic },
+		NeogitChangeDeleted = { fg = groups.git_delete, bold = styles.bold, italic = styles.italic },
+		NeogitChangeModified = { fg = groups.git_change, bold = styles.bold, italic = styles.italic },
+		NeogitChangeNewFile = { fg = groups.git_stage, bold = styles.bold, italic = styles.italic },
+		NeogitChangeRenamed = { fg = groups.git_rename, bold = styles.bold, italic = styles.italic },
+		NeogitChangeUpdated = { fg = groups.git_change, bold = styles.bold, italic = styles.italic },
 		NeogitDiffAddHighlight = { link = "DiffAdd" },
 		NeogitDiffContextHighlight = { bg = palette.surface },
 		NeogitDiffDeleteHighlight = { link = "DiffDelete" },
+		NeogitFilePath = { fg = palette.foam, italic = styles.italic },
 		NeogitHunkHeader = { bg = groups.panel },
 		NeogitHunkHeaderHighlight = { bg = groups.panel },
 
