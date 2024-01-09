0046bc7ead57300c995612e1097d7edc3618a64c
mvllow <mvllow@icloud.com>
2024-01-09 15:43:03 -0600

    add change copied

--- lua/rose-pine.lua
+++ lua/rose-pine.lua
@@ -391,7 +391,7 @@ local function set_highlights()
 		-- https://github.com/NeogitOrg/neogit/blob/master/lua/neogit/lib/hl.lua#L109-L198
 		NeogitChangeAdded = { fg = groups.git_add, bold = styles.bold, italic = styles.italic },
 		NeogitChangeBothModified = { fg = groups.git_change, bold = styles.bold, italic = styles.italic },
-		NeogitChangeCopied = { fg = palette.bg_cyan, bold = styles.bold, italic = styles.italic },
+		NeogitChangeCopied = { fg = groups.git_untracked, bold = styles.bold, italic = styles.italic },
 		NeogitChangeDeleted = { fg = groups.git_delete, bold = styles.bold, italic = styles.italic },
 		NeogitChangeModified = { fg = groups.git_change, bold = styles.bold, italic = styles.italic },
 		NeogitChangeNewFile = { fg = groups.git_stage, bold = styles.bold, italic = styles.italic },
