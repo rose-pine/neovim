dcf69a34fe7ea27222d89ef7d49696029116b4d1
jostaub <67969701+jostaub@users.noreply.github.com>
2024-08-02 19:29:28 +0200

    removed dead code forgotten in last commit

--- lua/rose-pine/palette.lua
+++ lua/rose-pine/palette.lua
@@ -65,15 +65,9 @@ local variants = {
 if options.palette ~= nil and next(options.palette) then
 	-- handle variant specific overrides
 	for variant_name, override_palette in pairs(options.palette) do
-		-- ignore pseudo variant all as is was allready handeld
-		if variant_name == "all" then
-			goto continue
-		end
-
 		if variants[variant_name] then
 			variants[variant_name] = vim.tbl_extend("force", variants[variant_name], override_palette or {})
 		end
-		::continue::
 	end
 end
 
