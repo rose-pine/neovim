46b2e6e56f2dde86a6cb66b2de57e10c0d8137fb
anupam-basu <anupamb@udel.edu>
2024-10-21 22:10:34 -0500

    adding ep walsh logic

---
+++ lua/rose-pine/plugins/obsidian.lua
@@ -0,0 +1,12 @@
+--- Rosé Pine for toggleterm
+--- https://github.com/epwalsh/obsidian.nvim
+---
+--- @usage
+--- local highlights = require("rose-pine.plugins.obsidian")
+--- require("obsidian").setup({ highlights = highlights })
+
+return {
+	ObsidianBullet = { fg = "muted" },
+	ObsidianRefText = { underline = true, fg = "iris" },
+	ObsidianDone = { bold = true, fg = "foam" }
+}
