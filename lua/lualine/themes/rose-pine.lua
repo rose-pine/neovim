fc418a716fa1808153da4bd53606f5524328f861
mvllow <mvllow@icloud.com>
2025-01-07 09:47:00 -0600

    fix: lualine transparency
    Fixes #333

--- lua/lualine/themes/rose-pine.lua
+++ lua/lualine/themes/rose-pine.lua
@@ -1,34 +1,40 @@
 local p = require("rose-pine.palette")
+local config = require("rose-pine.config")
+
+local bg_base = p.base
+if config.options.styles.transparency then
+	bg_base = "NONE"
+end
 
 return {
 	normal = {
 		a = { bg = p.rose, fg = p.base, gui = "bold" },
 		b = { bg = p.overlay, fg = p.rose },
-		c = { bg = p.base, fg = p.text },
+		c = { bg = bg_base, fg = p.text },
 	},
 	insert = {
 		a = { bg = p.foam, fg = p.base, gui = "bold" },
 		b = { bg = p.overlay, fg = p.foam },
-		c = { bg = p.base, fg = p.text },
+		c = { bg = bg_base, fg = p.text },
 	},
 	visual = {
 		a = { bg = p.iris, fg = p.base, gui = "bold" },
 		b = { bg = p.overlay, fg = p.iris },
-		c = { bg = p.base, fg = p.text },
+		c = { bg = bg_base, fg = p.text },
 	},
 	replace = {
 		a = { bg = p.pine, fg = p.base, gui = "bold" },
 		b = { bg = p.overlay, fg = p.pine },
-		c = { bg = p.base, fg = p.text },
+		c = { bg = bg_base, fg = p.text },
 	},
 	command = {
 		a = { bg = p.love, fg = p.base, gui = "bold" },
 		b = { bg = p.overlay, fg = p.love },
-		c = { bg = p.base, fg = p.text },
+		c = { bg = bg_base, fg = p.text },
 	},
 	inactive = {
-		a = { bg = p.base, fg = p.muted, gui = "bold" },
-		b = { bg = p.base, fg = p.muted },
-		c = { bg = p.base, fg = p.muted },
+		a = { bg = bg_base, fg = p.muted, gui = "bold" },
+		b = { bg = bg_base, fg = p.muted },
+		c = { bg = bg_base, fg = p.muted },
 	},
 }
